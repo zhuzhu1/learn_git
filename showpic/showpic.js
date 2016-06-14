72a66a10b41e58256539a65ed89998074fd0e973
gulingling <gulinglingcs@163.com>
2016-06-14 22:20:00 +0800

    add showpic

---
+++ showpic/showpic.js
@@ -0,0 +1,75 @@
+
+// function popUp(winURL)
+// {
+// 	window.open(winURL,"popup","width=300,height=480");
+// }
+
+function showPic(whichpic)
+{
+	if(!document.getElementById("placeholder")) return false;
+	var source = whichpic.getAttribute("href");
+	var placeholder=document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if(document.getElementById("discription"))
+	{
+		var text = whichpic.getAttribute("title");
+		var discription = document.getElementById("discription");
+		//discription.innerHTML = text;
+		//console.log(discription.childNodes[0].nodeValue);
+		discription.firstChild.nodeValue = text;
+	}
+	return true;
+	
+}
+
+// window.onload=prepareLinks;
+// function prepareLinks()
+// {
+// 	if(!document.getElementsByTagName) return false;
+// 	var links = document.getElementsByTagName("a");
+// 	for(var i=0;i<links.length;i++)
+// 	{
+// 		if(links[i].getAttribute("class") == "popup")
+// 		{
+// 			links[i].onclick=function(){
+// 				popUp(this.getAttribute("href"));
+// 				return false;
+// 			}
+// 		}
+// 	}
+// }
+
+//window.onload=prepareGallery;
+function prepareGallery()
+{
+	if(!document.getElementsByTagName || !document.getElementById) return false;
+	var gallery = document.getElementById("imagegallery");
+	if(!gallery) return false;
+
+	var links = gallery.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++)
+	{
+		links[i].onclick=function(){
+			return !showPic(this);
+		}
+	}
+
+}
+
+function addLoadEvent(func)
+{
+	//var oldonload = window.onload;
+	if(typeof window.onload != 'function')
+	{
+		window.onload = func;
+	}
+	else
+	{
+		window.onload = function(){
+			window.onload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(prepareGallery);
