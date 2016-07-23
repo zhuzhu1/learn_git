22865249c0d8b5377293c5d326d638bb7b657c73
gulingling <gulinglingcs@163.com>
2016-07-23 13:41:51 +0800

    js display insert sort

--- ifebaidu/task2-19/task.js
+++ ifebaidu/task2-19/task.js
@@ -1,8 +1,11 @@
+var numArr = [];
+var totalCount = 0;
+var ulQueue = document.getElementById("queue");
+var timeStamp = null;
+
 function init()
 {
-  var totalCount = 0;
-  var numArr = [];
-  var ulQueue = document.getElementById("queue");
+  
   var inputValue = document.getElementById("inputvalue");
 
   var leftIn = document.getElementById("leftin");
@@ -50,9 +53,9 @@ function init()
     if(ulQueue.childNodes.length > 0)
     {
       numArr.splice(0,1);
-      var delValue = ulQueue.firstChild.firstChild.nodeValue;
+      //var delValue = ulQueue.firstChild.firstChild.nodeValue;
       ulQueue.removeChild(ulQueue.firstChild);
-      alert("删除的数值为： " + delValue);
+      alert("删除的数值为： " + numArr[0]);
       totalCount--;
     }
   };
@@ -62,39 +65,97 @@ function init()
       if(ulQueue.childNodes.length > 0)
       {
          numArr.splice(totalCount-1,1);
-         var delValue = ulQueue.lastChild.firstChild.nodeValue;
+         //var delValue = ulQueue.lastChild.firstChild.nodeValue;
          ulQueue.removeChild(ulQueue.lastChild);
-         alert("删除的数值为： " + delValue);
+         alert("删除的数值为： " + numArr[totalCount - 1]);
          totalCount--;
       }
       
   };
 
   var sortObj = document.getElementById("sort");
+  // sortObj.onclick = function(){
+  //     for(var i = 1;i < totalCount; i++)
+  //     {
+  //         var temp = numArr[i];
+  //         var j = i - 1;
+  //         while(j >= 0 && numArr[j] > temp)
+  //         {
+  //             numArr[j + 1] = numArr[j];
+  //             //ulQueue.childNodes[j+1].style.height = numArr[j];
+  //             window.setTimeout(updateHeight,2000,j, numArr[j]);
+  //             j--;
+  //         }
+  //         numArr[j + 1] = temp;
+  //         //ulQueue.childNodes[j+1].style.height = temp;
+  //         window.setTimeout(updateHeight,2000,j, temp);
+  //     }
+  // };
+  
   sortObj.onclick = function(){
-      for(var i = 1;i < totalCount; i++)
+      //var i = 1, j = 0;
+      console.log(numArr);
+      if(numArr.length <= 1)
       {
-          var temp = numArr[i];
-          var j = i - 1;
-          while(j >= 0 && numArr[j] > temp)
-          {
-              numArr[j + 1] = numArr[j];
-              j--;
-          }
-          numArr[j + 1] = temp;
-
-          
-
+          return;
       }
+
+      var objIndex = {
+        i : 1,
+        j : 0,
+        temp : numArr[1]
+      };
+      timeStamp = null;
+      timeStamp = window.setInterval(updateHeight,300,objIndex);
+
   };
 
    ulQueue.addEventListener("click",function(event){
       if(event.target.nodeName == "LI")
       {
           ulQueue.removeChild(event.target);
+          totalCount--;
       }
    });
 }
+
+function updateHeight(objIndex)
+{
+    debugger;
+    if(objIndex.i < totalCount)
+    {
+        //var temp = numArr[objIndex.i];
+        if(objIndex.j >= 0 && numArr[objIndex.j] > objIndex.temp)
+        {
+            numArr[objIndex.j + 1] = numArr[objIndex.j];
+            ulQueue.childNodes[objIndex.j+1].style.height = numArr[objIndex.j];
+            objIndex.j--;
+        }
+        else
+        {
+            numArr[objIndex.j + 1] = objIndex.temp;
+            ulQueue.childNodes[objIndex.j+1].style.height = objIndex.temp;
+            objIndex.i++;
+            objIndex.j = objIndex.i - 1;
+            objIndex.temp = numArr[objIndex.i];
+        }
+    }
+    else
+    {
+        if(timeStamp != null)
+        {
+          window.clearInterval(timeStamp);
+          console.log(numArr);
+        }
+    }
+}
+
+// function updateHeight(j, value)
+// {
+//     var ulQueue = document.getElementById("queue");
+//     ulQueue.childNodes[j+1].style.height = value;
+// }
+
 
 function checkInput(inputValue)
 {
