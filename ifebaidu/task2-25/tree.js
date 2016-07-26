c795f1c3ca97b298e149ac67e45d29cc22a85605
gulingling <gulinglingcs@163.com>
2016-07-26 22:56:38 +0800

    25 over

--- ifebaidu/task2-25/tree.js
+++ ifebaidu/task2-25/tree.js
@@ -1,29 +1,11 @@
 //全局变量记得初始化，在前序遍历和后序遍历函数里也必须初始化
 var timeStamp = null;
 var isFind = false;
-var TRAVERSAL = "traversal";
-var SEARCH = "search";
 var selectedNode = [];
 
 function init()
 {
     var rootObj = document.getElementById("root");
-
-    var preOrderObj = document.getElementById("preOrder");
-    preOrderObj.onclick = function(){
-        var arr = [];
-        isFind = false;
-        preOrderTraversal(rootObj, arr);
-        show(arr, TRAVERSAL);
-    };
-
-    var afterOrderObj = document.getElementById("afterOrder");
-    afterOrderObj.onclick = function(){
-        var arr = [];
-        isFind = false;
-        afterOrderTraversal(rootObj, arr);
-        show(arr, TRAVERSAL);
-    };
 
     var queryInputObj = document.getElementById("queryInput");
     var queryBtnObj = document.getElementById("queryBtn");
@@ -32,93 +14,22 @@ function init()
         isFind = false;
         var inputValue = queryInputObj.value;
         preOrderQuery(rootObj, arr, inputValue);
-        show(arr, SEARCH);
+        show(arr);
     };
-
-    
-    //节点的折叠与展开
-    rootObj.addEventListener("dblclick",function(event){
-        debugger;
-        if(event.target.nodeName == "SPAN")
-        { 
-            var divs = event.target.parentNode.childNodes;
-            for(var i = 0; i < divs.length; i++)
-            {
-                // + -
-                if(divs[i].nodeName == "SPAN")
-                {
-                    if(divs[i].className == "")
-                    {
-                        divs[i].className="image";
-                    }
-                    else
-                    {
-                        divs[i].className = "";
-                    }
-                }
-                //show hide
-                if(divs[i].nodeName == "DIV")
-                {
-                    if(divs[i].className == "")
-                    {
-                        divs[i].className="hide";
-                    }
-                    else
-                    {
-                        divs[i].className = "";
-                    }
-                }
-                
-            }
-            event.stopPropagation();
-        }
-    });
 
     //点击某个节点元素，则该节点元素呈现一个特殊被选中的样式
     rootObj.addEventListener("mousedown",function(event){
-        //右键
-        if(event.button ==2){
-            if(this.className.indexOf("selected") >= 0)
-            {
-                var className = removeClass(this.className, "selected");
-                this.className= className;
-                var index = selectedNode.indexOf(this);
-                if(index > -1)
-                {
-                    selectedNode.splice(index,1);
-                }
-            }
-            else
-            {
-                this.className += " selected";
-                selectedNode.push(this);
-            }
-            event.stopPropagation();
+        //左键 节点的折叠与展开
+        if(event.button ==0){
+            foldOrNot(event);
         }
-
+        //右键 选中节点
+        if(event.button ==2){
+            chooseNode(event);
+            
+        }
+        event.stopPropagation();
     });
-    var divObjs = document.getElementsByTagName("div");
-    for(var i = 0, divslen = divObjs.length; i < divslen; i++)
-    {
-        divObjs[i].onclick = function(event){
-            if(this.className.indexOf("selected") >= 0)
-            {
-                var className = removeClass(this.className, "selected");
-                this.className= className;
-                var index = selectedNode.indexOf(this);
-                if(index > -1)
-                {
-                    selectedNode.splice(index,1);
-                }
-            }
-            else
-            {
-                this.className += " selected";
-                selectedNode.push(this);
-            }
-            event.stopPropagation();
-        };
-    }
 
     //删除
     var deleteObj = document.getElementById("delete");
@@ -140,43 +51,101 @@ function init()
         for(var i = 0, len = selectedNode.length; i < len; i++)
         {
             var divNode = document.createElement("div");
-            var divVal = document.createTextNode(addInputObj.value);
-            divNode.appendChild(divVal);
+            var spanNode = document.createElement("span");
+            var spanVal = document.createTextNode(addInputObj.value);
+            spanNode.appendChild(spanVal);
+            divNode.appendChild(spanNode);
             selectedNode[i].appendChild(divNode);
-            
         }
+
+        for(var j = 0; j < selectedNode.length; j++)
+        {
+            removeClass(selectedNode[j].firstChild, "selected");
+            removeClass(selectedNode[j].childNodes[3], "selected");
+        }
+        selectedNode = [];
     };
 }
 
-function preOrderTraversal(node, arr)
+function foldOrNot(event)
 {
-    if(node != null)
-    {
-        arr.push(node);
-        for(var i = 0, len = node.childNodes.length; i < len; i++)
+    if(event.target.nodeName == "SPAN")
+    { 
+        debugger;
+        var divs = event.target.parentNode.childNodes;
+        for(var i = 0; i < divs.length; i++)
         {
-            if(node.childNodes[i].nodeType == 1)
+            // + -
+            if(divs[i].nodeName == "SPAN")
             {
-                preOrderTraversal(node.childNodes[i], arr);
+                if(divs[i].className.indexOf("image") >= 0)
+                {
+                    removeClass(divs[i], "image");
+                }
+                else
+                {
+                    divs[i].className += " image";
+                }
             }
+            //show hide
+            if(divs[i].nodeName == "DIV")
+            {
+                if(divs[i].className.indexOf("hide") >= 0)
+                {
+                    removeClass(divs[i], "hide");
+                }
+                else
+                {
+                    divs[i].className="hide";
+                }
+            }
+            
         }
+        
     }
-    
 }
 
+function chooseNode(event)
+{
+    if(event.target.nodeName == "SPAN")
+    { 
+        var parentNode = event.target.parentNode;
+        if(event.target.className.indexOf("selected") >= 0)
+        {
+            removeClass(event.target, "selected");
+
+            var index = selectedNode.indexOf(parentNode);
+            if(index > -1)
+            {
+                selectedNode.splice(index,1);
+            }
+        }
+        else
+        {
+            event.target.className += " selected";
+            selectedNode.push(parentNode);
+        }
+    }
+}
+//arr里存放的是span
 function preOrderQuery(node, arr, inputValue)
 {
     if(node != null)
     {
-        arr.push(node);
+        //arr.push(node);
         
-        if(node.firstChild.nodeValue.trim() == inputValue)
-        {
-            isFind = true;
-        }
         for(var i = 0, len = node.childNodes.length; i < len; i++)
         {
-            if(isFind == false && node.childNodes[i].nodeType == 1)
+            if(node.childNodes[i].nodeName == "SPAN")
+            {
+                arr.push(node.childNodes[i]);
+                if(node.childNodes[i].firstChild.nodeValue.trim() == inputValue)
+                {
+                    isFind = true;
+                    break;
+                }
+            }
+            if(isFind == false && node.childNodes[i].nodeName == "DIV")
             {
                 preOrderQuery(node.childNodes[i], arr, inputValue);
             }
@@ -184,22 +153,7 @@ function preOrderQuery(node, arr, inputValue)
     }
 }
 
-function afterOrderTraversal(node, arr)
-{
-    if(node != null)
-    {
-        for(var i = 0, len = node.childNodes.length; i < len; i++)
-        {
-            if(node.childNodes[i].nodeType == 1)
-            {
-                afterOrderTraversal(node.childNodes[i], arr);
-            }
-        }
-        arr.push(node);
-    }
-}
-
-function show(arr, flag)
+function show(arr)
 {
     var indexObj = {
         i : 0
@@ -210,16 +164,14 @@ function show(arr, flag)
         window.clearInterval(timeStamp);
     }
 
-    timeStamp = window.setInterval(animation, 500, arr, indexObj, flag);
+    timeStamp = window.setInterval(animation, 500, arr, indexObj);
 }
 
-function animation(arr, indexObj, flag)
+function animation(arr, indexObj)
 {
     for(var j = 0; j < arr.length; j++)
     {
-        //arr[j].style.backgroundColor="white";
-        var className = removeClass(arr[j].className, "blueColor");
-        arr[j].className = className;
+        removeClass(arr[j], "blueColor");
     }
     if(indexObj.i == arr.length)
     {
@@ -229,40 +181,58 @@ function animation(arr, indexObj, flag)
         }
         if(isFind == true)
         {
-            //arr[indexObj.i - 1].style.backgroundColor = "blue";
             arr[indexObj.i - 1].className += " blueColor";
         }
         else
         {
-            if(flag == SEARCH)
+            alert("not found!"); 
+        }
+        
+        return;
+    }
+    arr[indexObj.i].className += " blueColor";
+    //打开子节点
+    openChild(arr[indexObj.i].parentNode);
+    indexObj.i++;
+    
+}
+
+function openChild(node)
+{
+    var childs = node.childNodes;
+    for(var i = 0; i < childs.length; i++)
+    {
+        if(childs[i].nodeName == "DIV")
+        {
+            if(childs[i].className.indexOf("hide") >=0)
             {
-                alert("not found!");
+                removeClass(childs[i], "hide");
             }
             
         }
-        return;
     }
-    //arr[indexObj.i].style.backgroundColor = "blue";
-    arr[indexObj.i].className += " blueColor";
-    indexObj.i++;
 }
 
-function removeClass(classStr, value)
+function removeClass(node, value)
 {
-    var className = "";
-    var arr = classStr.split(/\s+/);//以空格分割
-    for(var i = 0; i < arr.length; i++)
+    if(node != null)
     {
-        if(arr[i] != value)
+        var className = "";
+        var arr = node.className.split(/\s+/);//以空格分割
+        for(var i = 0; i < arr.length; i++)
         {
-            if(className != "")
+            if(arr[i] != value)
             {
-                className += " ";
+                if(className != "")
+                {
+                    className += " ";
+                }
+                className += arr[i];
             }
-            className += arr[i];
         }
+        node.className = className;
     }
-    return className;
+    
 }
 
 init();
